c269b07a17eee566e9b6099ea8f5a4b43cfacb69
Luis Coelho <mancoeluis@gmail.com>
2022-12-13 00:44:11 -0800

    ✨ add all email situations

--- src/htmlTemplateStrings.ts
+++ src/htmlTemplateStrings.ts
@@ -7,15 +7,16 @@ export function getTemplate(
   betsArray?: Bet[], 
   arrayOfAdminUsers?: User[]) 
   {
-  if (user.email !== 'admin@email.com'){
     switch (subject) {
       case SubjectEnum.newUser:
         return `<h1>Welcome to the Lottery API, ${user.name}!</h1>`
       case SubjectEnum.newPassword:
         return `<h1>Here's your token, ${user.name}</h1><p>Token: ${token}</p>`
+      case SubjectEnum.remindUserToBet:
+        return `<h1>It's been some time, ${user.name}!</h1><p>We've missed you! Why don't you take your chance and place a bet today?</p>`
       case SubjectEnum.newBet:
         return `<h1>Good luck, ${user.name}</h1><p>These were the bets you placed: ${betsArray!.map(bet => `<div style="display: inline-block;;border: 2px solid black;border-radius: 10px;"><p style="margin: 6px;">${bet.game}</p><p style="margin: 6px;">${bet.numbers}</p></div>`)}</p>`
+      default:
+        return `<h1>Admin, check out these new bets:</h1><p>${betsArray!.map(bet => `<div style="display: inline-block;;border: 2px solid black;border-radius: 10px;"><p style="margin: 6px;">${bet.game}</p><p style="margin: 6px;">${bet.numbers}</p></div>`)}`
     }
-  } 
-  return `<h1>Admin, check out these new bets:</h1><p>${betsArray!.map(bet => `<div style="display: inline-block;;border: 2px solid black;border-radius: 10px;"><p style="margin: 6px;">${bet.game}</p><p style="margin: 6px;">${bet.numbers}</p></div>`)}`
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Kafka } from 'kafkajs';
 import { TopicEnum } from './kafkaConnector'
-import { MessageFromProducer, SubjectEnum, User } from './msTypes';
+import { MessageFromProducer, SubjectEnum } from './msTypes';
 import { sendMail } from './sendMail';
 import 'dotenv/config'
 
@@ -20,8 +20,25 @@ const myMSConsumer = async () => {
     eachMessage: async ({ message }) => {
       const messageData: MessageFromProducer = JSON.parse(message.value!.toString())
       console.log(messageData)
-      if (messageData.subject === SubjectEnum.newBet) {
-        //await sendMail({user: admin, subject: SubjectEnum.newBet, betsArray: messageData.betsArray})
+
+      switch (messageData.subject) {
+        case SubjectEnum.newUser:
+          await sendMail({ user: messageData.user, subject: messageData.subject })
+        
+        case SubjectEnum.newBet:
+          await sendMail({ user: messageData.user, subject: messageData.subject, betsArray: messageData.betsArray})
+          await Promise.all(
+            messageData.arrayOfAdminUsers!.map(async admin => {
+              await sendMail({ user: admin, subject: `Admin, new bets were placed!`, betsArray: messageData.betsArray })
+          }))
+        case SubjectEnum.newPassword:
+          await sendMail({ user: messageData.user, subject: messageData.subject, token: messageData.token })
+        
+        case SubjectEnum.remindUserToBet:
+          await sendMail({ user: messageData.user, subject: messageData.subject })
+        
+        default:
+          break
       }
     },
   })
